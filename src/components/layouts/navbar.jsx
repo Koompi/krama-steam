5274cc262ffb876bb5e1b04a5f91a6b030b45274
san-vuthy <san.vuthy08@gmail.com>
2019-04-05 13:27:12 +0700

    update

--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import { Helmet } from "react-helmet";
+import Index from "../index";
 
 const drawerWidth = 240;
 
@@ -43,18 +40,6 @@ function Navbar(props) {
         <link rel="stylesheet" href="/css/style.css" />
       </Helmet>
       <CssBaseline />
-      {/* <AppBar position="fixed" className={classes.appBar + " navbarColor"}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            <img
-              src="img/koompi-logo-w-01.svg"
-              alt="koompi steam"
-              height="30"
-              className="koompi-steam-logo"
-            />
-          </Typography>
-        </Toolbar>
-      </AppBar> */}
       <Drawer
         className={classes.drawer}
         variant="permanent"
@@ -73,77 +58,94 @@ function Navbar(props) {
             </ListItem>
           ))} */}
 
-          <ListItem button>
+          <ListItem className="btnPadding" button>
             <img
-              src="https://img.icons8.com/ios/50/000000/code.png"
+              src="img/categories/development.png"
               alt="koompi steam"
               height="40"
               className="koompi-steam-logo"
             />
-            <ListItemText primary="Development" />
+            <p className="MuiTypography-subheading">Development</p>
           </ListItem>
-          <ListItem button>
+          <ListItem className="btnPadding" button>
             <img
-              src="https://img.icons8.com/ios/50/000000/code.png"
+              src="img/categories/business.png"
               alt="koompi steam"
               height="40"
               className="koompi-steam-logo"
             />
-            <ListItemText primary="Business" />
+            <p className="MuiTypography-subheading">Business</p>
           </ListItem>
-
-          <ListItem button>
+          <ListItem className="btnPadding" button>
             <img
-              src="https://img.icons8.com/ios/50/000000/code.png"
+              src="img/categories/IT.png"
               alt="koompi steam"
               height="40"
               className="koompi-steam-logo"
             />
-            <ListItemText primary="IT & Software" />
+            <p className="MuiTypography-subheading">IT & Software</p>
           </ListItem>
-
-          <ListItem button>
+          <ListItem className="btnPadding" button>
             <img
-              src="https://img.icons8.com/ios/50/000000/code.png"
+              src="img/categories/office.png"
               alt="koompi steam"
               height="40"
               className="koompi-steam-logo"
             />
-            <ListItemText primary="Development" />
+            <p className="MuiTypography-subheading">Office Productivity</p>
+          </ListItem>
+          <ListItem className="btnPadding" button>
+            <img
+              src="img/categories/personal.png"
+              alt="koompi steam"
+              height="40"
+              className="koompi-steam-logo"
+            />
+            <p className="MuiTypography-subheading">Personal Development</p>
+          </ListItem>
+          <ListItem className="btnPadding" button>
+            <img
+              src="img/categories/design.png"
+              alt="koompi steam"
+              height="40"
+              className="koompi-steam-logo"
+            />
+            <p className="MuiTypography-subheading">Design</p>
+          </ListItem>
+          <ListItem className="btnPadding" button>
+            <img
+              src="img/categories/marketing.png"
+              alt="koompi steam"
+              height="40"
+              className="koompi-steam-logo"
+            />
+            <p className="MuiTypography-subheading">Marketing</p>
+          </ListItem>
+          <ListItem className="btnPadding" button>
+            <img
+              src="img/categories/health.png"
+              alt="koompi steam"
+              height="40"
+              className="koompi-steam-logo"
+            />
+            <p className="MuiTypography-subheading">Health & Fitness</p>
+          </ListItem>
+          <ListItem className="btnPadding" button>
+            <img
+              src="img/categories/music.png"
+              alt="koompi steam"
+              height="40"
+              className="koompi-steam-logo"
+            />
+            <p className="MuiTypography-subheading">Music</p>
           </ListItem>
         </List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <div className={classes.root}>
+          <Index />
+        </div>
       </main>
     </div>
   );
